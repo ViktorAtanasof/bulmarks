a14528a20faaede7c04516a7b2a0069bc0ad02be
ViktorAtanasof <atanasowviktor@gmail.com>
2023-03-15 17:55:10 +0200

    Create routes and import react toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { Home } from './pages/Home';
 import { Landmarks } from './pages/Landmarks';
@@ -6,19 +8,33 @@ import { Profile } from './pages/Profile';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 import { ForgotPassword } from './pages/ForgotPassword';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/landmarks' element={<Landmarks />}/>
-          <Route path='/sign-in' element={<SignIn />}/>
-          <Route path='/sign-up' element={<SignUp />}/>
-          <Route path='/profile' element={<Profile />}/>
-          <Route path='/forgot-password' element={<ForgotPassword />}/>
-          <Route path='*' element={<h1>404 Not Found</h1>} />
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/landmarks' element={<Landmarks />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Routes>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 }
