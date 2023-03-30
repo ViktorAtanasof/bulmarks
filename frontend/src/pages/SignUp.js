cbd7a2dac9c184373a46894be9455cc464071eea
ViktorAtanasof <atanasowviktor@gmail.com>
2023-03-30 20:00:36 +0300

    Refactor code in SignUp and display errors when data validation fails

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -6,50 +6,47 @@ import { db } from '../firebase';
 import { serverTimestamp, setDoc, doc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import { OAuth } from '../components/OAuth';
+import { useForm } from 'react-hook-form';
 
 import lock from '../assets/images/lock.jpg';
 
-
 export const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const [formData, setFormData] = useState({
-        username: "",
-        email: "",
-        password: "",
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm({
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+        }
     });
-    
-    const { username, email, password } = formData;
-    const navigate = useNavigate();
 
-    const onChange = (e) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.id]: e.target.value,
-        }));
-    };
+    const navigate = useNavigate();
 
     const onClickPasswordIcon = () => {
         setShowPassword((prevState) => !prevState);
     };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+    const onChangeUsername = (e) => {
+        const trimmedUsername = e.target.value.replace(/\s/g, '');
+        setValue('username', trimmedUsername);
+    };
 
+    const onSubmit = async (data) => {
         try {
             const auth = getAuth();
-            const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-            const trimmedUsername = username.split(' ').join('');
+            const userCredentials = await createUserWithEmailAndPassword(auth, data.email, data.password);
             updateProfile(auth.currentUser, {
-                displayName: trimmedUsername,
+                displayName: data.username,
             })
             const user = userCredentials.user;
-            const formDataCopy = {...formData};
+            const formDataCopy = data;
             delete formDataCopy.password;
             formDataCopy.timestamp = serverTimestamp();
 
             await setDoc(doc(db, "users", user.uid), formDataCopy);
             navigate("/");
-        } catch(error) {
+        } catch (error) {
+            console.log(error);
             toast.error("Something went wrong.")
         }
     }
@@ -66,38 +63,88 @@ export const SignUp = () => {
                     />
                 </div>
                 <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-                    <form onSubmit={onSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <input
-                            className="w-full px-4 py-2 text-xl mb-6
+                            className={`w-full px-4 py-2 text-xl mb-4
                                      text-gray-700 bg-white border-gray-300 
-                                       rounded transition ease-in-out"
+                                       rounded transition ease-in-out
+                                       ${errors.username && 'border-red-600 border-1'}`}
                             type="text"
                             id="username"
                             placeholder="Username"
-                            value={username}
-                            onChange={onChange}
+                            {...register('username', {
+                                required: true,
+                                minLength: 3,
+                                maxLength: 20,
+                            })}
+                            onChange={onChangeUsername}
                         />
+                        {errors.username && (
+                            <div className='mb-4 px-1'>
+                                {errors.username.type === 'required' && (
+                                    <p className="text-red-500">Username can't be an empty string.</p>
+                                )}
+                                {errors.username.type === 'minLength' && (
+                                    <p className="text-red-500">Username must be at least 3 characters long.</p>
+                                )}
+                                {errors.username.type === 'maxLength' && (
+                                    <p className="text-red-500">Username must be less than 20 characters long.</p>
+                                )}
+                            </div>
+                        )}
                         <input
-                            className="w-full px-4 py-2 text-xl mb-6
+                            className={`w-full px-4 py-2 text-xl mb-4
                                      text-gray-700 bg-white border-gray-300 
-                                       rounded transition ease-in-out"
+                                       rounded transition ease-in-out
+                                       ${errors.email && 'border-red-600 border-1'}`}
                             type="email"
                             id="email"
                             placeholder="Email address"
-                            value={email}
-                            onChange={onChange}
+                            {...register('email', {
+                                required: true,
+                                pattern: /^[\w]+@[\w-]+\.+[\w-]{2,4}$/g,
+                            })}
                         />
-                        <div className="relative mb-6">
+                        {errors.email && (
+                            <div className='mb-4 px-1'>
+                                {errors.email.type === 'required' && (
+                                    <p className="text-red-500">Email can't be an empty string.</p>
+                                )}
+                                {errors.email.type === 'pattern' && (
+                                    <p className="text-red-500">
+                                        The email address you entered is not valid. It should be in the format <strong>username@example.com</strong>.
+                                    </p>
+                                )}
+                            </div>
+                        )}
+                        <div className="relative">
                             <input
-                                className="w-full px-4 py-2 text-xl 
+                                className={`w-full px-4 py-2 text-xl mb-4
                                      text-gray-700 bg-white border-gray-300 
-                                       rounded transition ease-in-out"
+                                       rounded transition ease-in-out
+                                       ${errors.password && 'border-red-600 border-1'}`}
                                 type={showPassword ? 'text' : 'password'}
                                 id="password"
                                 placeholder="Password"
-                                value={password}
-                                onChange={onChange}
+                                {...register('password', {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                })}
                             />
+                            {errors.password && (
+                                <div className='mb-4 px-1'>
+                                    {errors.password.type === 'required' && (
+                                        <p className="text-red-500">Password can't be an empty string.</p>
+                                    )}
+                                    {errors.password.type === 'minLength' && (
+                                        <p className="text-red-500">Password must be at least 6 characters long.</p>
+                                    )}
+                                    {errors.password.type === 'maxLength' && (
+                                        <p className="text-red-500">Password must be less than 20 characters long.</p>
+                                    )}
+                                </div>
+                            )}
                             {showPassword ? (
                                 <AiFillEyeInvisible
                                     className="absolute right-3 top-3 text-xl cursor-pointer"
