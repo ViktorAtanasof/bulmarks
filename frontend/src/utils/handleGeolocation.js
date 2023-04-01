7db02cf8a0b7ddc649acdb3506f138bc9b28bbf9
ViktorAtanasof <atanasowviktor@gmail.com>
2023-04-01 14:00:01 +0300

    Create handleGeolocation function

---
+++ frontend/src/utils/handleGeolocation.js
@@ -0,0 +1,15 @@
+export const handleGeolocation = async (data) => {
+    const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${data.address}
+        &key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+    );
+    const fetchedData = await response.json();
+    const geolocation = {
+        lat: fetchedData.results[0]?.geometry.location.lat ?? 0,
+        lng: fetchedData.results[0]?.geometry.location.lng ?? 0,
+    }
+
+    const location = fetchedData.status === 'ZERO_RESULTS' && undefined;
+
+    return {geolocation, location};
+};
