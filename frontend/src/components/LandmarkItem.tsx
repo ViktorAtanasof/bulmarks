cbe102ee2598be350e2ffbf511ac2da7152b7d1b
ViktorAtanasof <atanasowviktor@gmail.com>
2024-02-11 11:39:54 +0200

    Style the LandmarkItem component for light and dark theme

--- frontend/src/components/LandmarkItem.tsx
+++ frontend/src/components/LandmarkItem.tsx
@@ -17,8 +17,8 @@ export const LandmarkItem = ({
     onEdit,
 }: LandmarkItemProps) => {
     return (
-        <li className="relative bg-white flex flex-col justify-between items-center 
-                       shadow-md hover:shadow-xl rounded-md overflow-hidden
+        <li className="relative bg-primary-color flex flex-col justify-between items-center 
+                       shadow-md shadow-box-color hover:shadow-box-color hover:shadow-xl rounded-md overflow-hidden
                        transition-shadow duration-150 m-[10px]">
             <Link to={`/category/${landmark.size}/${id}`} className="contents">
                 <img
@@ -32,13 +32,13 @@ export const LandmarkItem = ({
                     <div className="flex items-center space-x-1">
                         <MdLocationOn className="h-4 w-4 text-green-600" />
                         <p
-                            className="font-semibold text-sm mb-[2px] text-gray-600 truncate"
+                            className="font-semibold text-sm mb-[2px] text-gray-600 dark:text-gray-300 truncate"
                         >
                             {landmark.place}
                         </p>
                     </div>
-                    <p className="font-semibold mt-0 text-xl truncate">{landmark.name}</p>
-                    <p className="text-[#457b9d] mt-2 font-semibold">{landmark.type}</p>
+                    <p className="font-semibold mt-0 text-xl text-secondary-color truncate">{landmark.name}</p>
+                    <p className="text-[#457b9d] dark:text-[#5dacdc] mt-2 font-semibold">{landmark.type}</p>
                 </div>
             </Link>
             {onDelete && (
@@ -49,7 +49,7 @@ export const LandmarkItem = ({
             )}
             {onEdit && (
                 <MdEdit
-                    className="absolute bottom-2 right-7 h-4 cursor-pointer"
+                    className="absolute bottom-2 right-7 h-4 cursor-pointer text-secondary-color"
                     onClick={() => onEdit(landmark.id)}
                 />
             )}
