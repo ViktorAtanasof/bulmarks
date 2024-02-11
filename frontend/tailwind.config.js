d70e46763b279122e635b8cbe153d4f38770d2d2
ViktorAtanasof <atanasowviktor@gmail.com>
2024-02-11 12:55:58 +0200

    Configure light dark color variable for tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         "primary-color": "var(--primary-color)",
         "secondary-color": "var(--secondary-color)",
         "accent-color": "var(--accent-color)",
-        "box-color": "var(--box-color)",
+        "ldark-color": "var(--ldark-color)",
       }
     },
   },
