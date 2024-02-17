5372d3773f3463d3117a3703f6ba5ecd43537c8c
ViktorAtanasof <atanasowviktor@gmail.com>
2024-02-17 14:43:00 +0200

    Style the NotFound page for light and dark themes

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 export const NotFound = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8 bg-blue-50 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-700 sm:text-4xl lg:text-5xl">
+        <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8 bg-primary-color
+        transition-all duration-500 ease-in-out sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-secondary-color sm:text-4xl lg:text-5xl">
                 404 Not Found
             </h1>
-            <p className="max-w-md mt-4 text-center text-gray-500 sm:text-lg lg:text-xl sm:mt-6 lg:mt-8">
+            <p className="max-w-md mt-4 text-center text-gray-500 dark:text-gray-300 sm:text-lg lg:text-xl sm:mt-6 lg:mt-8">
                 The page you are looking for does not exist.
             </p>
             <Link
