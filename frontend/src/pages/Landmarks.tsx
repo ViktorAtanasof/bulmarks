1e8ddf3c36acae147c25947cd63160273c5fbee8
ViktorAtanasof <atanasowviktor@gmail.com>
2024-03-02 14:36:26 +0200

    Refactor code for the Landmarks page (improvements and opimizations)

--- frontend/src/pages/Landmarks.tsx
+++ frontend/src/pages/Landmarks.tsx
@@ -1,4 +1,12 @@
-import { DocumentData, collection, getDocs, limit, orderBy, query, startAfter } from "firebase/firestore";
+import {
+  DocumentData,
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { LandmarkItem } from "../components/LandmarkItem";
@@ -7,102 +15,102 @@ import { db } from "../firebase";
 import { LandmarkData } from "../types/landmarkTypes";
 
 export const Landmarks = () => {
-    const [landmarks, setLandmarks] = useState<LandmarkData[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [lastFetchedLandmark, setLastFetchedLandmark] = useState<DocumentData | null>(null);
-    const [hasMoreLandmarks, setHasMoreLandmarks] = useState(false);
+  const [landmarks, setLandmarks] = useState<LandmarkData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [lastFetchedLandmark, setLastFetchedLandmark] =
+    useState<DocumentData | null>(null);
+  const [hasMoreLandmarks, setHasMoreLandmarks] = useState(false);
 
-    useEffect(() => {
-        const fetchLandmarks = async () => {
-            try {
-                const landmarkRef = collection(db, 'landmarks');
-                const q = query(
-                    landmarkRef,
-                    orderBy('timestamp', 'desc'),
-                    limit(8),
+  const fetchLandmarks = async (fetchMore = false) => {
+    try {
+      const landmarkRef = collection(db, "landmarks");
+      let q;
+      if (fetchMore) {
+        q = query(
+          landmarkRef,
+          orderBy("timestamp", "desc"),
+          startAfter(lastFetchedLandmark),
+          limit(4)
+        );
+      } else {
+        q = query(landmarkRef, orderBy("timestamp", "desc"), limit(8));
+      }
+      const querySnap = await getDocs(q);
+      setHasMoreLandmarks(querySnap.size >= 8);
+      const fetchedLandmarks: LandmarkData[] = [];
+      querySnap.forEach((doc) => {
+        fetchedLandmarks.push({
+          id: doc.id,
+          data: doc.data() as LandmarkData["data"],
+        });
+      });
+      if (fetchMore) {
+        setLandmarks((prevLandmarks) => [
+          ...prevLandmarks,
+          ...fetchedLandmarks,
+        ]);
+      } else {
+        setLandmarks(fetchedLandmarks);
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedLandmark(lastVisible);
+      }
+      setLoading(false);
+    } catch (error) {
+      toast.error("Could not fetch landmarks");
+      console.error("Could not fetch landmarks:", error);
+    }
+  };
+
+  // fetchLandmarks is not changing between renders
+  useEffect(() => {
+    fetchLandmarks();
+    // eslint-disable-next-line
+  }, []);
+
+  const handleFetchMoreLandmarks = () => {
+    fetchLandmarks(true);
+  };
+
+  return (
+    <div className="max-w-6xl mx-auto px-3">
+      <h1 className="text-3xl text-center mt-6 mb-6 font-bold text-secondary-color">
+        All landmarks
+      </h1>
+      {loading ? (
+        <Spinner />
+      ) : landmarks?.length > 0 ? (
+        <>
+          <main>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {landmarks.map((landmark) => {
+                return (
+                  <LandmarkItem
+                    key={landmark.id}
+                    id={landmark.id}
+                    landmark={landmark.data}
+                  />
                 );
-                const querySnap = await getDocs(q);
-                setHasMoreLandmarks(querySnap.size >= 8);
-                const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-                setLastFetchedLandmark(lastVisible);
-                const landmarks: LandmarkData[] = [];
-                querySnap.forEach((doc) => {
-                    return landmarks.push({
-                        id: doc.id,
-                        data: doc.data() as LandmarkData['data'],
-                    });
-                });
-                setLandmarks(landmarks);
-                setLoading(false);
-            } catch (error) {
-                toast.error('Could not fetch landmarks.');
-            }
-        };
-        fetchLandmarks();
-    }, []);
-
-    const onFetchMoreLandmarks = async () => {
-        try {
-            const landmarkRef = collection(db, 'landmarks');
-            const q = query(
-                landmarkRef,
-                orderBy('timestamp', 'desc'),
-                startAfter(lastFetchedLandmark),
-                limit(4),
-            );
-            const querySnap = await getDocs(q);
-            setHasMoreLandmarks(querySnap.size >= 4);
-            const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-            setLastFetchedLandmark(lastVisible);
-            const landmarks: LandmarkData[] = [];
-            querySnap.forEach((doc) => {
-                return landmarks.push({
-                    id: doc.id,
-                    data: doc.data() as LandmarkData['data'],
-                });
-            });
-            setLandmarks((prevState) => {
-                return [...prevState, ...landmarks]
-            });
-            setLoading(false);
-        } catch (error) {
-            toast.error('Could not fetch landmarks.');
-        }
-    };
-
-    return (
-        <div className="max-w-6xl mx-auto px-3">
-            <h1 className="text-3xl text-center mt-6 mb-6 font-bold text-secondary-color">All landmarks</h1>
-            {loading ? (
-                <Spinner />
-            ) : landmarks?.length > 0 ? (
-                <>
-                    <main>
-                        <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-                            {landmarks.map((landmark) => {
-                                return <LandmarkItem
-                                    key={landmark.id}
-                                    id={landmark.id}
-                                    landmark={landmark.data}
-                                />
-                            })}
-                        </ul>
-                    </main>
-                    {hasMoreLandmarks && (
-                        <div className="flex justify-center items-center">
-                            <button
-                                onClick={onFetchMoreLandmarks}
-                                className="bg-secondary-color px-3 py-1.5 text-primary-color border 
+              })}
+            </ul>
+          </main>
+          {hasMoreLandmarks && (
+            <div className="flex justify-center items-center">
+              <button
+                onClick={handleFetchMoreLandmarks}
+                className="bg-secondary-color px-3 py-1.5 text-primary-color border 
                                            mb-6 mt-6 hover:text-accent-color rounded
                                            transition duration-300 ease-in-out"
-                            >
-                                Load more</button>
-                        </div>
-                    )}
-                </>
-            ) : (
-                <p className="text-2xl text-center italic my-12 text-secondary-color">There are no landmarks available.</p>
-            )}
-        </div>
-    );
+              >
+                Load more
+              </button>
+            </div>
+          )}
+        </>
+      ) : (
+        <p className="text-2xl text-center italic my-12 text-secondary-color">
+          There are no landmarks available.
+        </p>
+      )}
+    </div>
+  );
 };
